b74ddf1f781036408d6f6a8815877b6d92e289e9
BartoszBakCL <bartosz.bak@poczta.onet.pl>
2021-12-10 19:42:34 +0100

    adding contact section

--- Oddam w dobre ręce/src/components/homepage-components/HomeContact.js
+++ Oddam w dobre ręce/src/components/homepage-components/HomeContact.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Title from "../common/Title";
+
 
 const HomeContact = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="contact" className="contact">
+            <div className="contact__form">
+                <Title title="Skontaktuj się z nami"/>
+                <form>
+                    <label htmlFor="name">Wpisz swoje imię</label>
+                    <input type="text" required id="name" name="name" placeholder="Krzysztof"/>
+                    <label htmlFor="email">Wpisz swój email</label>
+                    <input id="email" name="email" placeholder="abc@xyz.pl" type="email" required/>
+                    <label htmlFor="message">Wpisz swoją wiadomość</label>
+                    <input type="text" required id="message" name="message" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
+                    <button className="button"><span>Wyślij</span></button>
+                </form>
+            </div>
+        </section>
     );
 };
 
