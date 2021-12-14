59d9e5c59ec7595beb71c2484a5e055be43b8215
BartoszBakCL <bartosz.bak@poczta.onet.pl>
2021-12-14 18:54:30 +0100

    reformat code

--- Oddam w dobre ręce/src/components/pages/Logout.js
+++ Oddam w dobre ręce/src/components/pages/Logout.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
 import Title from "../common/Title";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Logout = () => {
+  const stylesLogout = {
+    padding: "100px",
+    display: "grid",
+    "place-items": "center",
+  };
 
-    const stylesLogout = {
-        "padding": "100px",
-        "display": "grid",
-        "place-items": "center"
-    }
-
-    return (
-        <section style={stylesLogout} className="logout">
-            <Title title="Wylogowanie nastąpiło pomyślnie!" />
-            <Link to="/" className="button">Strona główna</Link>
-        </section>
-    );
+  return (
+    <section style={stylesLogout} className="logout">
+      <Title title="Wylogowanie nastąpiło pomyślnie!" />
+      <Link to="/" className="button">
+        Strona główna
+      </Link>
+    </section>
+  );
 };
 
 export default Logout;
